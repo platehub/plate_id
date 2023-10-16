348cc61e3a60b4f8b8499f0414e1beb671dbba6f
Elena Freudenberg <elena@getplate.com>
2023-10-16 12:10:00 +0200

    edit test and add raise for mapping that does not exist yet

--- lib/uri/plate_id.rb
+++ lib/uri/plate_id.rb
@@ -104,7 +104,7 @@ module URI
         return unless comps
 
         parts = MAPPING[comps[:model_name]].dup
-        return unless parts
+        raise "Mapping is missing for this class"
 
         parts[:scheme] = comps[:scheme]
         parts[:id] = comps[:model_id]
